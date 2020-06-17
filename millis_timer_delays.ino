a17c6f122ed710396185e49003dcd2dc5e7ac976
Jesse Harding <jesse.alexander.harding@gmail.com>
2020-06-17 18:55:43 -0400

    Update millis_timer_delays.ino

--- millis_timer_delays.ino
+++ millis_timer_delays.ino
@@ -22,21 +22,21 @@ void loop(){
   
 
 
-////LETS START WITH A BLINK SKETCH USING DELAY()
-//  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(interval);                       // wait for a tenth of a second
-//  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-//  delay(interval);                       // wait for a tenth of a second
+//LETS START WITH A BLINK SKETCH USING DELAY()
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(interval);                       // wait for a tenth of a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(interval);                       // wait for a tenth of a second
 
 
 
-//NOW LETS USE A MILLIS() TIMER INSTEAD OF A DELAY
-  if (millis() - prevMillis > interval){ //compare current time against stored time; if the difference is greater than our interval variable, the code within will run
-    digitalWrite(ledPin, ledState); //write the ledState to our ledPin
-    ledState = !ledState; //toggle the led state between true & false
-    prevMillis = millis(); //set stored time to the current time to reset the timer (now the difference between prevMillis and the current time will be less than the interval)
-  }
-  Serial.println(millis()); //print the number of milliseconds since the program began to the serial connection
+////NOW LETS USE A MILLIS() TIMER INSTEAD OF A DELAY
+//  if (millis() - prevMillis > interval){ //compare current time against stored time; if the difference is greater than our interval variable, the code within will run
+//    digitalWrite(ledPin, ledState); //write the ledState to our ledPin
+//    ledState = !ledState; //toggle the led state between true & false
+//    prevMillis = millis(); //set stored time to the current time to reset the timer (now the difference between prevMillis and the current time will be less than the interval)
+//  }
+//  Serial.println(millis()); //print the number of milliseconds since the program began to the serial connection
 
 
 
